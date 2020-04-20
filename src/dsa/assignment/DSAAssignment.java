3ff3c192fc08f891a4b348b9ac5d553bf0f7e942
user <user@LAPTOP-SETN5R1I.Home>
2020-04-20 18:35:17 +0100

    Initial check

---
+++ src/dsa/assignment/DSAAssignment.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa.assignment;
+
+/**
+ *
+ * @author user
+ */
+public class DSAAssignment {
+
+    public Int getTrackId(){
+        return trackId;
+    }
+    public void setTrackId(Int newTrackId){
+        this.trackId = newTrackId;
+    }
+    
+    public String getTrackTitle(){
+        return trackTitle;
+    }
+    public void setTrackTitle(String newTrackTitle){
+        this.trackTitle = newTrackTitle;
+    }
+    
+    public String getArtist(){
+        return artist;
+    }
+    public void setArtist(String newArtist){
+        this.artist = newArtist;
+    }
+    
+    public String getTrackLength(){
+        return trackLength;
+    }
+    public void setTrackLength(String newTrackLength){
+        this.trackLength = newTrackLength;
+    }
+    
+    public String getComposer(){
+        return composer;
+    }
+    public void setComposer(String newComposer){
+        this.composer = newComposer;
+    }
+    
+    public String getReleaseDate(){
+        return releaseDate;
+    }
+    public void setReleaseDate(String newReleaseDate){
+        this.releaseDate = newReleaseDate;
+    }
+    
+    public String getAlbum(){
+        return album;
+    }
+    public void setAlbum(String newAlbum){
+        this.album = newAlbum;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public void setGenre(String newGenre){
+        this.genre = newGenre;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
